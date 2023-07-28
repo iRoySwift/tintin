c0683e0b7190d2e255b71d726097d54a86cc2a74
Roy <iRoySwift@iCloud.com>
2023-07-28 10:56:40 +0800

    string new

--- rust_five_class/src/lib.rs
+++ rust_five_class/src/lib.rs
@@ -1,5 +1,8 @@
 #[macro_export]
 macro_rules! string {
+    () => {
+        String::new()
+    };
     ($a:expr) => {{
         let string = String::from($a);
         string
@@ -8,6 +11,10 @@ macro_rules! string {
 
 #[cfg(test)]
 mod test {
+    #[test]
+    fn test_empty_string() {
+        assert_eq!(string!(""), String::new());
+    }
     #[test]
     fn test_string() {
         assert_eq!(string!("Hello Rust!"), String::from("Hello Rust!"));
